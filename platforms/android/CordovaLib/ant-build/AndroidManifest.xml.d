81400faf60f3640f5d1436c21797880fecfba1eb
Keith Spencer <keithspencer@mycwi.cc>
2015-03-20 14:35:36 -0600

    Initial commit of project 4 files

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\sandbox\webd265\ch05\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\sandbox\webd265\ch05\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\sandbox\webd265\ch05\platforms\android\ant-build\AndroidManifest.xml : \
+c:\sandbox\webd265\ch05\platforms\android\AndroidManifest.xml \
